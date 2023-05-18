f8503d4954074d521cd3ba1ac19bc1c8b9e6e06b
Brian <wadekaka3@outlook.com>
2023-05-18 20:59:42 +0800

    First Commit

---
+++ src/main/java/com/itheima/reggie/service/EmployeeService.java
@@ -0,0 +1,11 @@
+package com.itheima.reggie.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.itheima.reggie.entity.Employee;
+
+/**
+ * @author Brianxq
+ * @version 1.0
+ */
+public interface EmployeeService extends IService<Employee> {
+}
